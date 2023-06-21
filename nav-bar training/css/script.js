16d246634d95389ac158b839476796522f83788e
OluwaZhang <johnzhangdammy.c@gmail.com>
2023-06-20 21:12:59 -0700

    study upgrade

---
+++ nav-bar training/css/script.js
@@ -0,0 +1,13 @@
+// const menuIcon = document.querySelector(".hamburger-menu");
+// const navbar = document.querySelector(".navbar");
+// menuIcon.addEventListener("click", () => {
+//     navbar.classList.toggle("change")
+// });
+
+const menuIcon = document.querySelector(".hamburger-menu");
+
+const navbar = document.querySelector(".nav-bar")
+
+menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("new")
+})
